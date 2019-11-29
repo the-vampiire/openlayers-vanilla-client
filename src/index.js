452ebc5c0afb09ac39958a543db98b1ee0d6b6e9
Vamp <vampiirecodes@gmail.com>
2019-11-28 21:19:45 -0600

    client: completed part 1 objectives

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import {} from "./utils.js";
+import buildEndpoint from "./api-utils.js";
+import { createBaseLayer, createGeoJSONReportsLayer } from "./map-utils.js";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const map = new ol.Map({
+    target: "map",
+    view: new ol.View({
+      zoom: 2,
+      center: [0, 0],
+    }),
+    layers: [createBaseLayer()],
+  });
+
+  map.addLayer(createGeoJSONReportsLayer({ url: buildEndpoint("/reports") }));
+});
