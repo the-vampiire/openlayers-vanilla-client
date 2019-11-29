3243970fe1a658abf954041ec9b11093277c3fd2
Vamp <vampiirecodes@gmail.com>
2019-11-28 21:18:56 -0600

    client/map-utils: builder functions
    createBaseLayer
    createVectorLayer
    createCirclestyle
    createGeoJSONReportsLayer

---
+++ src/map-utils.js
@@ -0,0 +1,35 @@
+export const createBaseLayer = (source = new ol.source.OSM()) =>
+  new ol.layer.Tile({ source });
+
+export const createVectorLayer = ({ url, style, format }) =>
+  new ol.layer.Vector({
+    style,
+    source: new ol.source.Vector({ url, format }),
+  });
+
+export const createGeoJSONReportsLayer = ({
+  url,
+  style = createCircleStyle({
+    color: "red",
+    radius: 2,
+  }),
+}) =>
+  createVectorLayer({
+    url,
+    style,
+    format: new ol.format.GeoJSON(),
+  });
+
+export const createCircleStyle = ({
+  color,
+  stroke,
+  radius,
+  fill = new ol.style.Fill({ color }),
+}) =>
+  new ol.style.Style({
+    image: new ol.style.Circle({
+      fill,
+      radius,
+      stroke,
+    }),
+  });
