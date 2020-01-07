c7f8e170bd370030e642ba9e1c01c418fdb009df
Vamp <vampiirecodes@gmail.com>
2020-01-06 18:52:18 -0600

    root: complete refactor creates map, layer, and search tools

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,21 +1,40 @@
-import { endpointBuilder } from "./api-utils.js";
-import { displayFeatureReports } from "./event-handlers.js";
-import { createBaseLayer, createGeoJSONReportsLayer } from "./map-utils.js";
+import API_ORIGIN from "./api-origin.js";
+import createSearchBox from "./dom/search-box.js";
+import createDateSelect from "./dom/date-select.js";
+import {
+  endpointBuilder,
+  buildReportsByDateEndpoint,
+  buildReportsBySearchEndpoint,
+} from "./api-utils.js";
+import {
+  createZikaMap,
+  createGeoJSONReportsLayer,
+} from "./ol-zika/map-utils.js";
+import {
+  updateFeaturesBySearch,
+  updateFeaturesByDateChange,
+} from "./ol-zika/map-updaters.js";
 
-const API_URL = "http://localhost:8080";
-const buildEndpoint = endpointBuilder(API_URL);
+const buildEndpoint = endpointBuilder(API_ORIGIN);
+const reportsByDateEndpoint = buildReportsByDateEndpoint(buildEndpoint);
+const reportsBySearchEndpoint = buildReportsBySearchEndpoint(buildEndpoint);
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const map = new ol.Map({
-    target: "map",
-    view: new ol.View({
-      zoom: 2,
-      center: [0, 0],
-    }),
-    layers: [createBaseLayer()],
+  const map = createZikaMap();
+  const reportsLayer = createGeoJSONReportsLayer({});
+
+  map.addLayer(reportsLayer);
+
+  await createDateSelect({
+    buildEndpoint,
+    handleDateChange: updateFeaturesByDateChange(
+      reportsLayer,
+      reportsByDateEndpoint,
+    ),
   });
 
-  map.addLayer(createGeoJSONReportsLayer({ url: buildEndpoint("/reports") }));
-
-  map.on("click", displayFeatureReports);
+  await createSearchBox({
+    buildEndpoint,
+    handleSearch: updateFeaturesBySearch(reportsLayer, reportsBySearchEndpoint),
+  });
 });
