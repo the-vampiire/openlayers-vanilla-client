dbb4dada7a487aaa8b0f9ef1bff9a7a3ca78e5b2
Vamp <vampiirecodes@gmail.com>
2020-01-06 18:47:27 -0600

    ol-zika: moved map utils here

---
+++ src/modules/ol-zika/map-utils.js
@@ -0,0 +1,34 @@
+const createVectorLayer = ({ url, style, format }) =>
+  new ol.layer.Vector({
+    style,
+    source: new ol.source.Vector({ url, format }),
+  });
+
+const createBaseLayer = (source = new ol.source.OSM()) =>
+  new ol.layer.Tile({ source });
+
+export const createZikaMap = ({
+  zoom = 3,
+  layers = [createBaseLayer()],
+  center = ol.proj.fromLonLat([-74.063644, 4.624335]),
+} = {}) =>
+  new ol.Map({
+    target: "map",
+    view: new ol.View({
+      zoom,
+      center,
+    }),
+    layers,
+  });
+
+export const createGeoJSONReportsLayer = ({
+  url,
+  style = ol.style.Stroke({
+    stroke: "red",
+  }),
+}) =>
+  createVectorLayer({
+    url,
+    style,
+    format: new ol.format.GeoJSON(),
+  });
