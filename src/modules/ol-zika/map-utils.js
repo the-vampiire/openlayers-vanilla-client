51a5f21f948d19f1e774f3c85c564f1e70e6e947
Vamp <vampiirecodes@gmail.com>
2020-01-07 22:33:31 -0600

    ol-zika/utils: create vector source from url and features

--- src/modules/ol-zika/map-utils.js
+++ src/modules/ol-zika/map-utils.js
@@ -1,8 +1,30 @@
-const createVectorLayer = ({ url, style, format }) =>
-  new ol.layer.Vector({
-    style,
-    source: new ol.source.Vector({ url, format }),
+import createLocationToggles from "../dom/location-toggles.js";
+
+export const createVectorSourceFromURL = config => {
+  const { url } = config;
+
+  const vectorSource = new ol.source.Vector({
+    url,
+    format: new ol.format.GeoJSON(),
   });
+
+  vectorSource.on("change", () => {
+    if (vectorSource.getState() === "ready") {
+      createLocationToggles(vectorSource.getFeatures());
+    }
+  });
+
+  return vectorSource;
+};
+
+export const createVectorSourceFromFeatures = config => {
+  const { features } = config;
+
+  return new ol.source.Vector({
+    features,
+    format: new ol.format.GeoJSON(),
+  });
+};
 
 const createBaseLayer = (source = new ol.source.OSM()) =>
   new ol.layer.Tile({ source });
@@ -17,7 +39,8 @@ export const createZikaMap = ({
     view: new ol.View({
       zoom,
       center,
-      projection: "EPSG:4326",
+      // projection: "EPSG:4326",
+      projection: "EPSG:3857",
     }),
     layers,
   });
@@ -28,8 +51,7 @@ export const createGeoJSONReportsLayer = ({
     stroke: "red",
   }),
 }) =>
-  createVectorLayer({
-    url,
+  new ol.layer.Vector({
     style,
-    format: new ol.format.GeoJSON(),
+    source: new ol.source.Vector({ url, format: new ol.format.GeoJSON() }),
   });
