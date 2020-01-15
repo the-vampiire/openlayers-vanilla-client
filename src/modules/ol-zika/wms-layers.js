bc6bd1bac978d54a83ebdccb1a9c1ff13d80e4bb
Vamp <vampiirecodes@gmail.com>
2020-01-15 15:16:30 -0600

    ol-zika/wms layers: set opacity, remove dead layer link

--- src/modules/ol-zika/wms-layers.js
+++ src/modules/ol-zika/wms-layers.js
@@ -1,8 +1,9 @@
-import { createLayerToggle } from "./map-utils.js";
+import { createLayerVisibilityToggle } from "./map-utils.js";
 
 const wmsLayerCreator = map => (url, LAYERS) =>
   new ol.layer.Image({
     map,
+    opacity: 0.5,
     visible: false,
     source: new ol.source.ImageWMS({
       url,
@@ -26,11 +27,6 @@ export default map => {
     ["sdei:sdei-global-summer-lst-2013_day-max-global"],
   );
 
-  const nightSurfaceTemp2013Layer = createWMSLayer(
-    "https://sedac.ciesin.columbia.edu/geoserver/wms",
-    ["sdei:sdei-global-summer-lst-2013_night-min-global"],
-  );
-
   const freshwaterAvailabilityLayer = createWMSLayer(
     "https://sedac.ciesin.columbia.edu/geoserver/wms",
     ["sdei:sdei-trends-freshwater-availability-grace"],
@@ -39,23 +35,20 @@ export default map => {
   return [
     {
       layer: populationDensity2015Layer,
-      name: "Population Density (2015)",
-      toggleVisibility: createLayerToggle(populationDensity2015Layer),
+      name: "Population Density [2015]",
+      toggleVisibility: createLayerVisibilityToggle(populationDensity2015Layer),
     },
     {
       layer: daySurfaceTemp2013Layer,
-      name: "Surface Temperature 2013 [day]",
-      toggleVisibility: createLayerToggle(daySurfaceTemp2013Layer),
-    },
-    {
-      layer: nightSurfaceTemp2013Layer,
-      name: "Surface Temperature 2013 [night]",
-      toggleVisibility: createLayerToggle(nightSurfaceTemp2013Layer),
+      name: "Daytime Surface Temperature [2013]",
+      toggleVisibility: createLayerVisibilityToggle(daySurfaceTemp2013Layer),
     },
     {
       layer: freshwaterAvailabilityLayer,
-      name: "Freshwater Availability 2002-16",
-      toggleVisibility: createLayerToggle(freshwaterAvailabilityLayer),
+      name: "Freshwater Availability [2002-16]",
+      toggleVisibility: createLayerVisibilityToggle(
+        freshwaterAvailabilityLayer,
+      ),
     },
   ];
 };
