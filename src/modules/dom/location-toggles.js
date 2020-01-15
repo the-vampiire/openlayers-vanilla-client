3644472d7dcbdeb508a779ca2ffb5759b690f8d0
Vamp <vampiirecodes@gmail.com>
2020-01-15 09:25:21 -0600

    ol-zika/location toggles: group by country and add count info

--- src/modules/dom/location-toggles.js
+++ src/modules/dom/location-toggles.js
@@ -1,4 +1,5 @@
 import { createElement } from "./utils.js";
+import { sortFeaturesByCount } from "../ol-zika/data-utils.js";
 import calculatedFeatureStyle, {
   invisibleFeatureStyle,
 } from "../ol-zika/feature-style.js";
@@ -21,6 +22,14 @@ const buildLocationTitle = locationFeature => {
   return `${state}, ${country}`;
 };
 
+const buildCountInfoString = locationFeature => {
+  const { cases, reports } = locationFeature.getProperties();
+
+  const infoString = reports ? `${reports.length} reports` : `${cases} cases`;
+
+  return `<span class="count-info-string">${infoString}</span>`;
+};
+
 /**
  *
  * @param {ol.Feature} locationFeature
@@ -28,6 +37,8 @@ const buildLocationTitle = locationFeature => {
 const createLocationToggle = locationFeature => {
   const location = buildLocationTitle(locationFeature);
   const id = buildLocationFeatureId("location-toggle", locationFeature);
+
+  const countInfoString = buildCountInfoString(locationFeature);
 
   const locationToggle = createElement({
     id,
@@ -52,18 +63,54 @@ const createLocationToggle = locationFeature => {
 
   return createElement({
     tag: "div",
-    children: [locationToggle, `<label for=${id}>${location}</label>`],
+    children: [
+      locationToggle,
+      `<label for=${id}>${location}: ${countInfoString}</label>`,
+    ],
   });
 };
 
+const convertToCountryToggles = countryGroups =>
+  Object.entries(countryGroups).map(countryGroupEntry => {
+    const [country, features] = countryGroupEntry;
+
+    // sort by cases / reports count
+    features.sort(sortFeaturesByCount);
+
+    // convert to HTML toggle elements
+    const locationToggles = features.map(createLocationToggle);
+
+    return createElement({
+      id: `${country}-location-toggles`,
+      children: [`<h3>${country}</h3>`, ...locationToggles],
+    });
+  });
+
+const createCountryLocationToggles = locationFeatures =>
+  locationFeatures.reduce((countryGroups, locationFeature, index) => {
+    const { country } = locationFeature.getProperties();
+
+    // use existing group or create a new array group for the country toggles
+    countryGroups[country] = countryGroups[country] || [];
+
+    // push a location toggle for the feature belonging to the country
+    // countryGroups[country].push(createLocationToggle(locationFeature));
+    countryGroups[country].push(locationFeature);
+
+    return index !== locationFeatures.length - 1
+      ? // until the last element continue to accumulate the location toggles by country
+        countryGroups
+      : // on the final element convert the country groups to HTML elements
+        convertToCountryToggles(countryGroups);
+  }, {});
+
 export default locationFeatures => {
+  const countryLocationToggles = createCountryLocationToggles(locationFeatures);
+
   const locationToggles = createElement({
     tag: "div",
     id: "location-toggles",
-    children: [
-      "<h2>Toggle Locations</h2>",
-      ...locationFeatures.map(createLocationToggle),
-    ],
+    children: ["<h2>Toggle Locations</h2>", ...countryLocationToggles],
   });
 
   TOGGLES_CONTAINER.innerHTML = "";
